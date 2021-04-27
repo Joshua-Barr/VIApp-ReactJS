a7f4393449a45dc845581d721ed523cfaca6ef80
Joshua <Joshbarrscience@gmail.com>
2021-04-27 16:55:36 +0800

    Added trackers and built home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,16 @@ import "../styles.css";
 
 export default function Home() {
     
+    const home_class = "home" 
+    const linkedin= "https://au.linkedin.com/in/joshua-barr-03049a10b"
+
     return (
-      <div>
-        Welcome to the Value Investing Application <br/><br/>
-        This Web App is intended to be used to help evaluate stocks based on funamentals related to long term growth.
+      <div class={home_class}>
+        There are not many places on the internet that offer long term stock evaluations based on fundamental indicators.<br/><br/>
+        This tool aims to solve this problem by offering an easy and effective way to analyse individual stocks. <br/><br/><br/>
+        
+        Please use the Menu to select an Index or use one of the provided utilities...<br/><br/>
+        <div><a href={linkedin} >Developed by Joshua Barr</a></div>
       </div>
     );
 }
