778e4d44f33d1cce24b6959e292c70c7f97e7da3
Joshua <Joshbarrscience@gmail.com>
2021-05-19 22:09:54 +0800

    Removed spinner

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,10 @@ export default function Home() {
 
     return (
       <div class={home_class}>
-        There are not many places on the internet that offer long term stock evaluations based on fundamental indicators.<br/><br/>
-        This tool aims to solve this problem by offering an easy and effective way to analyse individual stocks. <br/><br/><br/>
+        There are not many places on the internet that offer long term stock evaluations based on fundamental indicators.<br/>
+        This tool aims to solve this problem by offering an easy and effective way to analyse individual stocks. <br/><br/>
         
-        Use this tool at your own risk. Please use the Menu to select an Index or use one of the provided utilities...<br/><br/>
+        Use this tool at your own risk. Please use the Menu to select an Index or use one of the provided utilities...<br/>
         <div><a href={linkedin} >Developed by Joshua Barr</a></div>
       </div>
     );
